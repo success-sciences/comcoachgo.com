5b5dc80cb4ab7329a16e92eaf5465bb7133e77b0
success-sciences <bharris@success-sciences.com>
2018-02-12 10:51:17 -0500

    new site

---
+++ share-your-requirements.php
@@ -0,0 +1,250 @@
+<?php
+/*
+ *  Template name: share your requirements
+ */
+ session_start();
+	 //~ $_SESSION['amp_pageval'] = $_REQUEST['pageval'];
+	 //~ $_SESSION['ampfname'] = $_REQUEST['ampfname'];
+	 //~ $_SESSION['ampemail'] = $_REQUEST['ampemail'];
+	 //~ $_SESSION['ampmobile'] = $_REQUEST['ampmobile'];
+	 //~ $_SESSION['ampmessage'] = $_REQUEST['ampmessage'];
+	 //~ $_SESSION['ampcost'] = $_REQUEST['ampcost'];
+
+?>
+<!DOCTYPE html>
+   <html lang="en" amp><head>
+       <meta charset="utf-8">
+       <meta content="IE=edge">
+       
+       <meta name="description" content="">
+       <meta name="author" content="">
+   
+      <title>NMG Form</title>
+      
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+ 
+  <noscript>
+  <style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style>
+  </noscript>
+  <meta name="viewport" content="width=device-width,minimum-scale=1">
+  <link rel="canonical" href="https://nmgtechnologies.com/get-custom-pricing">
+<style amp-custom>
+@font-face{font-family:'Gotham-Medium';src:url('<?php bloginfo( 'template_url' ); ?>/amp/fonts/Gotham-Medium.eot') format("embedded-opentype");font-weight:400;font-style:normal}@font-face{font-family:'Gotham-Medium';src:url('<?php bloginfo( 'template_url' ); ?>/amp/fonts/Gotham-Medium.otf') format("opentype"),url('<?php bloginfo( 'template_url' ); ?>/amp/fonts/Gotham-Medium.woff') format("woff"),url('<?php bloginfo( 'template_url' ); ?>/amp/fonts/Gotham-Medium.ttf') format("truetype"),url(Gotham-Medium.svg#Gotham-Medium) format("svg");font-weight:400;font-style:normal}@font-face{font-family:'sf_ui_textlight';src:url('<?php bloginfo( 'template_url' ); ?>/amp/fonts/sf-ui-text-light_0-webfont.woff2') format("woff2"),url('<?php bloginfo( 'template_url' ); ?>/amp/fonts/sf-ui-text-light_0-webfont.woff') format("woff");font-weight:400;font-style:normal}*{margin:0;padding:0;box-sizing:border-box}div{cursor:default}::selection{background:#0077b5;color:#fff}::-moz-selection{background:#0077b5;color:#fff}body,html{overflow-x:hidden;margin:0;padding:0;font:12px Gotham-Medium}h1,h2,h3,h4,h5,h6{padding:0;margin:0}.lt{float:left}.rt{float:right}.cb{clear:both}h4{font-weight:400}.wrapper{margin:0 auto;width:1024px;padding:20px 0 60px}header{background-color:#6f818a;height:auto}.toptxt{font-size:20px;font-family:'sf_ui_textlight';color:#fff;-webkit-appearance:none}.logo{text-align:center}.banner{padding:120px 0 40px;text-align:center}.banner h1{font-size:43px;color:#fff;text-align:center;letter-spacing:-1px;text-shadow:5px 2px 5px #65727a;line-height:44px;margin-bottom:30px}.frmSubmit .buttonSent{background-color:#eb2344;font-size:18px;padding:20px 60px;color:#fff;text-decoration:none;border-radius:5px;margin-top:28px;display:inline-block;font-weight:400;width:100%}.txtarea{border:1px solid #e2e2e4;border-radius:10px;background-color:#fff;padding:20px;width:100%;font-size:15px;font-family:'sf_ui_textlight';color:#232323;margin-top:14px;height:98px}.inputBox{margin-top:14px}.inputBox input{border:1px solid #e2e2e4;border-radius:10px;background-color:#fff;padding:20px;width:23.95%}.inputBox input:nth-child(2){margin:0 5px 0 10px}.inputBox input:nth-child(3){margin:0 10px 0 5px}.frmSubmit{text-align:center}.frmSubmit p{font-size:15px;font-family:'sf_ui_textlight';color:#252525;margin-top:42px}#form{height:100%;background-color:#6e7e87}.wrapperfrm{width:500px;margin:0 auto;padding:34px 60px;background-color:#f1f2f3;border-radius:5px}.wrapperfrm .inputBox{margin-top:4px}.wrapperfrm .inputBox input{margin:14px 0 0;width:100%;border-radius:5px;font-size:17px}.wrapperfrm .inputBox .txtarea{font-size:17px}.wrapperfrm .inputBox input:nth-child(2){margin:14px 0 0}.wrapperfrm h3{font-size:19px;margin-bottom:0;font-weight:400;text-align:center}#form .banner{font-size:38px;font-family:'sf_ui_textlight';color:#fff;padding:0 0 20px}.wrapperfrm .frmSubmit .mybtn{background-color:#eb2344;font-size:18px;color:#fff;padding:16px;width:100%;display:block;text-decoration:none;border-radius:5px;margin-top:28px;appearance:none;-moz-appearance:none;-webkit-appearance:none;border:none}.frmLogo{text-align:center;margin-top:50px}.frmLogo span{margin:0 20px}.mainfrm h3{font-size:17px;margin-bottom:20px}.mainfrm .frmSubmit a{margin-top:20px;width:100%}.ratingBar{background-color:#45545c;padding:15px 0 8px;font-family:'sf_ui_textlight';font-size:15px;color:#fff;display:block}.ratingContent{width:1024px;margin:0 auto;position:relative}.footerLogo{position:absolute;right:190px;top:-32px}.ratingContent span:nth-child(1){float:left}.ratingContent span:nth-child(2){float:right}.bar2{background-color:#687379;padding:15px 0}.bar3{background-color:#fff;padding:15px 0;color:#45545c}.socialLinks a{padding:0 4px}.logoM{display:none;text-align:center}.footerphoneno{display:none}.toptxtfotlog{display:none}.ratingContent a.privacy{color:#fff;font-size:14px;float:right}@media screen and (max-width:1024px){.wrapper,.ratingContent{width:1000px}}@media screen and (max-width:768px){.wrapper,.ratingContent{width:730px}.logo{margin-top:40px}header{height:100%}.banner{padding:84px 0 40px}.banner h1{font-size:60px;line-height:64px}.chkBoxwrp li{width:100%;min-height:100%;height:auto}.chkBoxwrp li:nth-child(2){margin:14px 0}.inputBox input{width:100%}.inputBox input:nth-child(2){margin:14px 0 0}.inputBox input:nth-child(3){margin:14px 0}.footerLogo{position:relative;right:auto;top:auto;text-align:center}}@media screen and (max-width:767px){.wrapper,.ratingContent{width:96%}.lt,.rt{float:none}.toptxt{text-align:center;margin-bottom:15px}.wrapper{padding:36px 0}header{height:auto}.banner h1{font-size:36px;line-height:40px}.banner a{font-size:16px;padding:20px 30px}.banner{padding:46px 0 20px}.feedback li{width:100%}.feedback li:nth-child(2){border-top:1px solid #f6f6f6;margin-top:20px;border-left:none}.ratingContent{text-align:center}.ratingContent span:nth-child(1),.ratingContent span:nth-child(2){float:none}.ratingContent span{display:block;padding:10px 0}.wrapperfrm{width:100%;padding:28px 20px}#form .banner{padding:0 0 20px;font-size:20px}.logoM{display:block;margin-bottom:12px;margin-top:14px}.frmLogo span{margin:0 10px}.logoBig{display:none}.wrapperfrm .inputBox input{margin:6px 0 0}.wrapperfrm .inputBox input:nth-child(2){margin:6px 0 0}.wrapperfrm .inputBox input:nth-child(3){margin:6px 0 0}.wrapperfrm .inputBox .txtarea{margin-top:6px}#form .wrapper{padding:20px 0 36px}.inputBox input{padding:12px}.wrapperfrm .frmSubmit .mybtn{margin-top:10px}.footerphoneno{display:block;font-size:16px;font-family:'Gotham-Medium';text-align:center;padding:18px 0 0;color:#000;-moz-appearance:none;-webkit-appearance:none;appearance:none}.footerphoneno a{color:#eb2344;text-decoration:none}.toptxtfotlog{display:block;text-align:center}.footerLogos,.wrapperfrm h3{display:none}.bar2 .ratingContent{text-align:left}*{font-weight:400}}
+.sectionTab{display:none;}
+.inputBox input{padding:15px;}
+.select-style{margin-top:14px;}
+.select-style select {
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    background-color: rgba(0,0,0,0);
+    background-image: none;
+    border: medium none;
+    box-shadow: none;
+    color: #777;
+    font-size: 17px;
+    padding: 14px 25px 14px 12px;
+    width: 100%;
+    background: #fff url(http://demo.nmgtechnologies.com/wp-content/themes/whiteboard/amp/amp-img/selectArrow.png) no-repeat scroll 96% center;
+border: 1px solid #e2e2e4; border-radius:5px;} 
+.wrapperfrm{padding:24px 40px 38px 40px;}
+.wrapperfrm .inputBox .txtarea{font-family:"Helvetica Neue",Helvetica,Arial,sans-serif; font-size:17px; color:#777; padding:13px;}
+ </style>
+  
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+     <script async custom-element="amp-form" src="https://cdn.ampproject.org/v0/amp-form-0.1.js"></script>
+  <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>
+ <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+ </head>
+<body>
+	<amp-analytics config="https://www.googletagmanager.com/amp.json?id=GTM-TB9SRKG&gtm.url=SOURCE_URL" data-credentials="include"></amp-analytics>
+    <div id="form" class="section1">
+    <div class="wrapper">
+        
+            <div class="toptxt logoBig lt"><a href="nmgtechnologies.com"><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/logo.png" alt="" width="154" height="56"></amp-img></a></div>
+			<div class="logoM"><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/logoM.png" alt="" width="90" height="33"></amp-img></div>
+            <div class="toptxt rt footerLogos"><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/footerLogos.png" alt="" width="174" height="90"></amp-img></div>
+            <div class="cb"></div>
+            <div class="banner">
+                <p>Great! We look forward to chatting<br>and learning more about your goals.</p>
+                
+            </div>
+        
+         
+    
+        <div class="wrapperfrm">
+        <!--<h3>Please let us know can we contact you for the custom pricing?</h3>-->
+        <form method="GET" target="_top" action="" name="registration">
+        <div class="inputBox">
+            <input type="text" class="txt" name="ampfname" placeholder="Full name*" required /> 
+            <input type="email" class="txt" name="ampemail" placeholder="Email*" required />
+            <input type="text" class="txt" name="ampmobile" placeholder="Phone*" required />
+            
+			<div class="select-style">
+				<select name="budget" required />
+				<option value="">Budget*</option>
+				<option value="£10,000">£10,000</option>
+				<option value="10-£25000">10-£25000</option>
+				<option value="25-£50,000">25-£50,000</option>
+				<option value="50-£100,000">50-£100,000</option>
+				<option value=">£100,000">>£100,000</option>
+				</select>  
+			</div>
+            <div class="select-style">
+				<select name="organisation" required />
+				<option value="">Organisation*</option>
+				<option value="B2B">B2B</option>
+				<option value="B2C">B2C</option>
+				<option value="SMB">SMB</option>
+				<option value="Enterprise">Enterprise</option>
+				<option value="Startup">Startup</option>
+				<option value="Agency">Agency</option>
+				</select>  
+            </div>
+			<div class="select-style">
+				<select name="services" required />
+				<option value="">Service looking for*</option>
+				<option value="Web application development">Web application development</option>
+				<option value="Mobile application development">Mobile application development</option>
+				<option value="Website design">Website design</option>
+				<option value="Portal development">Portal development</option>
+				<option value="SAAS app">SAAS app</option>
+				<option value="Maintenance">Maintenance</option>
+				<option value="Website redesign">Website redesign</option>
+				</select> 
+			</div>
+            
+			<textarea class="txtarea" name="ampmessage" id="message" placeholder="Share your project requirements" required /></textarea>
+        </div>        
+        <div class="frmSubmit">
+            <!--a href="#">Send My Custom Pricing</a-->
+            <input type="hidden" name="share_requirements" value="true" />	
+             <button type="submit" class="mybtn">Send My Custom Pricing</button>
+            </div>
+        </div>
+        </form>
+            
+           <div class="frmLogo">
+                <span><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/frm-logo1.png" alt="" width="151" height="49"></amp-img></span>
+                <span><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/frm-logo2.png" alt="" width="140" height="49"></amp-img></span>
+                <span><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/frm-logo3.png" alt="" width="147" height="49"></amp-img></span>
+                <span><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/frm-logo4.png" alt="" width="124" height="49"></amp-img></span>
+                <span><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/frm-logo5.png" alt="" width="150" height="49"></amp-img></span>
+            </div>
+ </div>              
+    </div>
+
+    <div class="footer">
+            <div class="ratingBar">
+                <div class="ratingContent">
+                <span>Rated 4.9* (<amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/stars.png" alt=""  width="84" height="15"></amp-img>) for web & software development services by our clients</span>
+                <span class="socialLinks">
+                    <a class="facebook" href="https://www.facebook.com/newmediaguru" target="_blank"><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/fb.png" alt="" width="21" height="20"></amp-img></a>
+                    <a class="twitter" href="https://twitter.com/new_media_guru" target="_blank"><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/twt.png" alt="" width="21" height="20"></amp-img></a>
+                    <a class="linkedin" href="https://www.linkedin.com/company/new-media-guru-nmg-" target="_blank"><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/in.png" alt="" width="21" height="20"></amp-img></a>
+                    <a class="gplus" href="https://plus.google.com/+newmediaguru" target="_blank"><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/gplus.png" alt="" width="21" height="20"></amp-img></a>
+                </span>
+                <div class="cb"></div>
+                <div class="toptxtfotlog"><amp-img src="<?php bloginfo( 'template_url' ); ?>/amp/amp-img/footerLogos.png" alt="" width="174" height="90"></amp-img></div>
+                </div>
+        </div>
+            </div>
+        <div class="ratingBar bar2">
+            <div class="ratingContent">Copyright &copy; 2008 - 2017 NMG <a class="privacy" href="https://nmgtechnologies.com/privacy-policy/">Privacy Policy</a></div>
+        </div>
+		<div class="footerphoneno">Talk to an Expert: 1-800-884-0336</div>
+        <div class="ratingBar bar3">
+            <div class="ratingContent">304 S. Jones Blvd #3799, Las Vegas NV 89107 </div>
+        </div>
+    </div>
+    
+    <!-----popup------>
+    <?php if($_REQUEST['id']=='1') {?>
+		<script>
+		 $(window).load(function(){        
+   $('#myModal').modal('show');
+    }); 
+		</script> 
+   <div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+<?php }?>
+    <!-----popup------>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>     
+<!-- Start of Async Drift Code -->
+<script>
+!function() {
+  var t;
+  if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0, 
+  t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+  t.factory = function(e) {
+    return function() {
+      var n;
+      return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
+    };
+  }, t.methods.forEach(function(e) {
+    t[e] = t.factory(e);
+  }), t.load = function(t) {
+    var e, n, o, i;
+    e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"), 
+    o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js", 
+    n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
+  });
+}();
+drift.SNIPPET_VERSION = '0.3.1';
+drift.load('rtrzdfkmpdht');
+</script>
+<!-- End of Async Drift Code -->
+
+
+
+<script type="text/javascript">
+    window._mfq = window._mfq || [];
+    (function() {
+        var mf = document.createElement("script");
+        mf.type = "text/javascript"; mf.async = true;
+        mf.src = "//cdn.mouseflow.com/projects/9708e56a-e002-43d2-8c40-6867a41b42bc.js";
+        document.getElementsByTagName("head")[0].appendChild(mf);
+    })();
+</script>
+ 
+</body>
+
+</html>
+
+
+
+
+
+ 
+         
+         
+
+
+
+
+
+
+
